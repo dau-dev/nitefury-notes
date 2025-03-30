acc5193c8bca8ff2b0c56253b1e7d920ca111bbc
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-03-29 23:23:19 -0400

    .

--- amaranth_example.py
+++ amaranth_example.py
@@ -1,6 +1,31 @@
 from amaranth_boards.nitefury import NitefuryIIPlatform
-from amaranth import Elaboratable, Module, Signal, Cat
+from amaranth import (
+    Elaboratable,
+    Module,
+    Instance,
+    ClockSignal,
+    ResetSignal,
+    Signal,
+    Cat,
+)
 from amaranth.lib import io
+
+
+VERILOG_SOURCE = """
+module counter (
+  input clk,
+  input rst,
+  output logic[15:0] out
+);
+
+  always_ff @ (posedge clk) begin
+    if (rst)
+      out <= 0;
+    else
+      out <= out + 1;
+  end
+endmodule
+"""
 
 
 class Chaser(Elaboratable):
@@ -12,12 +37,21 @@ class Chaser(Elaboratable):
         leds = [io.Buffer("o", platform.request("led", i, dir="-")) for i in range(4)]
         m.submodules += leds
 
-        timer = Signal(range(int(clk_freq//8)), init=0)
+        size = range(int(clk_freq // 8))
+        timer = Signal(size, init=0)
         flop_1 = Signal(1, init=1)
         flop_2 = Signal(1, init=1)
         flop_3 = Signal(1, init=1)
         flop_4 = Signal(1, init=0)
 
+        m.submodules.counter = Instance(
+            "counter",
+            i_clk=ClockSignal(),
+            # i_rst=ResetSignal(),
+            o_out=timer,
+        )
+
+        platform.add_file("counter.sv", VERILOG_SOURCE)
         m.d.comb += Cat(led.o for led in leds).eq(Cat(flop_1, flop_2, flop_3, flop_4))
 
         with m.If(timer == 0):
@@ -25,7 +59,9 @@ class Chaser(Elaboratable):
             m.d.sync += flop_2.eq(flop_1)
             m.d.sync += flop_3.eq(flop_2)
             m.d.sync += flop_4.eq(flop_3)
-        m.d.sync += timer.eq(timer + 1)
+
+        # m.d.sync += timer.eq(timer + 1)
         return m
+
 
 NitefuryIIPlatform().build(Chaser(), do_program=True)
